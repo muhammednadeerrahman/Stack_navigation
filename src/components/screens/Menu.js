d47ec83259ba61cfb00b25629150eb2ccdf6c8da
muhammednadeerrahman <mohdnadeerrahman@gmail.com>
2024-01-11 13:01:08 +0530

    menu page navigation added

--- src/components/screens/Menu.js
+++ src/components/screens/Menu.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const {width, height} = Dimensions.get('screen')
 
-export default function Menu() {
+export default function Menu({navigation}) {
     return (
         <SafeAreaView style={styles.Main}>
                 <Text style={styles.title}>Menu Page</Text>
